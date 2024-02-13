0ee19048cd00ebc3d545850eea920ba36214e98a
jeffubayi <jeffubayi@gmail.com>
2024-02-13 10:01:24 +0300

    Initial Commit

---
+++ event-planner/event_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_plannerApp.swift
+//  event-planner
+//
+//  Created by Jeff Ubayi on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct event_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
