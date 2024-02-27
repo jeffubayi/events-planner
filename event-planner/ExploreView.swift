001b62fa889760d9c11d2fb977c8c6b83baec75b
jeffubayi <jeffubayi@gmail.com>
2024-02-27 20:22:33 +0300

    update ui

---
+++ event-planner/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  event-planner
+//
+//  Created by Jeff Ubayi on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
